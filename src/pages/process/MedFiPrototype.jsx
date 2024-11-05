471f6c3abdb6cf056cf620b6a3f4af69d6cc4284
alisonkr <alisonkr@stanford.edu>
2024-11-04 16:51:19 -0800

    Update MedFiPrototype.jsx

--- src/pages/process/MedFiPrototype.jsx
+++ src/pages/process/MedFiPrototype.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import Link from "@material-ui/core/Link";
 
 const Container = styled.div`
   display: flex;
@@ -25,6 +26,12 @@ const MedFiPrototype = () => {
   return (
     <Container>
       <Title>MedFiPrototype!</Title>
+      <Link
+        href="https://www.figma.com/proto/fOpsMCTOCVcIZfkPUBPBQ9/Gloo?node-id=126-1643&node-type=canvas&t=KMCFKSR24AQLD6BN-1&scaling=scale-down&content-scaling=fixed&page-id=126%3A1626&starting-point-node-id=126%3A1627"
+        onClick={() => {
+            alert("Redirecting to Figma Prototype");
+        }}
+      >
       <StyledIframe
         src="https://drive.google.com/file/d/1vNN0htBDS7cg40f1lM9bA1Atqapl7emg/view"
         width="1280"
